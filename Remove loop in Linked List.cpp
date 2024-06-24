6eee96bd857fb167d9bbb89f4f91ed695f7da413
Md .  Shah Alam <94603993+MdShah-alam@users.noreply.github.com>
2024-06-24 22:18:25 +0600

    Add files via upload

--- Remove loop in Linked List.cpp
+++ Remove loop in Linked List.cpp
@@ -6,128 +6,96 @@ class Node
 public:
     int data;
     Node *next;
-
-    Node(int data)
-    {
+    Node(int data){
         this->data=data;
         this->next=NULL;
     }
 };
 
-Node *insert_at_tail(Node *&head, int val)
+Node *insert_at_tail(Node *&head , int val)
 {
-    if(head==NULL)
-    {
-        head=new Node(val);
-        return head;
-    }
-
+    if(head==NULL) return head=new Node(val);
     Node *temp=head;
-    while(temp->next!=NULL)
-    {
+    while(temp->next!=NULL){
         temp=temp->next;
     }
-
-    Node *newNode = new Node(val);
-    temp->next=newNode;
+    temp->next=new Node(val);
     return head;
 }
 
 void print(Node *head)
 {
     Node *temp=head;
-    while(temp)
-    {
+    while(temp){
         cout<<temp->data<<" ";
         temp=temp->next;
     }
+    cout<<endl;
 }
 
-Node *createcycle(Node *&head, int val)
+void removeLoop(Node *head)
+{
+    Node *slow=head;
+    Node *fast=head;
+
+    while(slow!=NULL && fast!=NULL && fast->next!=NULL){
+        fast=fast->next->next;
+        slow=slow->next;
+        if(slow==fast){
+            break;
+        }
+    }
+    if(slow==head){
+        while(fast->next!=slow)
+            fast=fast->next;
+        fast->next=NULL;
+    }
+    else if(slow==fast){
+        slow=head;
+        while(slow->next!=fast->next){
+            slow=slow->next;
+            fast=fast->next;
+        }
+        fast->next=NULL;
+    }
+}
+
+void loop(Node *head , int val)
 {
     Node *temp=head;
-    Node *valnode=head;
-    while(temp->next!=NULL)
-    {
+    Node *temp2;
+
+    while(temp->next!=NULL){
         if(temp->data==val)
-            valnode=temp;
+            temp2=temp;
         temp=temp->next;
     }
 
-    temp->next=valnode;
-    return head;
+    temp->next=temp2;
 }
-
-bool checkcycle(Node *head)
-{
-    Node *fast=head;
-    Node *slow=head;
-
-    while(fast!=NULL && fast->next!=NULL)
-    {
-        fast=fast->next->next;
-        slow=slow->next;
-        if(fast==slow)
-            return true;
-    }
-    return false;
-}
-
-void removeLoop(Node* head)
-{
-    Node * fast=head;
-    Node * slow=head;
-
-    do
-    {
-        cout<<fast->data<<" "<<slow->data<<endl;
-        fast=fast->next->next;
-        slow=slow->next;
-
-    }while(fast!=slow);
-    cout<<fast->data<<" "<<slow->data<<endl;
-    cout<<endl<<"After"<<endl;
-    fast=head;
-
-    while(fast->next!=slow->next)
-    {
-        cout<<fast->data<<" "<<slow->data<<endl;
-        fast=fast->next;
-        slow=slow->next;
-    }
-    cout<<fast->data<<" "<<slow->data<<endl;
-    slow->next=NULL;
-
-}
-
 
 int main()
 {
-    Node *head=NULL;
     int n;
-    cout<<"How many node : ";
     cin>>n;
+    Node *head=NULL;
 
-    for(int i=0; i<n; i++)
-    {
+    for(int i=0;i<n;i++){
         int a;
         cin>>a;
-        head=insert_at_tail(head,a);
+        head = insert_at_tail(head,a);
     }
-    cout<<endl;
     print(head);
-
-    int val;
-    cout<<endl<<"Enter a number to create cycle : ";
-    cin>>val;
-
-    head = createcycle(head,val);
-
-    cout<<checkcycle(head);
-    cout<<endl;
+    cout<<"loop start point : ";
+    int k;
+    cin>>k;
+    loop(head,k);
     removeLoop(head);
-    cout<<checkcycle(head);
-    cout<<endl;
-
     return 0;
 }
+/**
+
+5
+1 2 3 4 5
+
+*/
